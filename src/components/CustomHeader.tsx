7bf3d4743b5b791608fb1401e3e14e7eb882710c
cron-ix <74101164+cron-ix@users.noreply.github.com>
2025-12-10 16:45:21 +0100

    Deutsche Übersetzung CustomHeader.tsx

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Head from "next/head";
 
-const title = "Enshrouded Skill Planner";
-const description = "A skill tree planner for the game Enshrouded.";
+const title = "Enshrouded Fähigkeiten Planer";
+const description = "Ein Fähigkeiten-Baum Planer für das Spiel Enshrouded.";
 
 const CustomHeader = () => {
   return (
@@ -11,11 +11,6 @@ const CustomHeader = () => {
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1232366016476149"
-        crossOrigin="anonymous"
-      ></script>
       {/* <meta property="og:url" content={baseUrl} /> */}
     </Head>
   );
