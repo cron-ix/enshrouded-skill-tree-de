f396f1daeac70590792ad24118bab8e3524b5de3
cron-ix <74101164+cron-ix@users.noreply.github.com>
2025-12-10 16:52:48 +0100

    Deutsche Übersetzung _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="de">
       <Head>
-        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
-          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-        )}
       </Head>
       <body>
         <Main />
